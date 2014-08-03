33f6647b5c52af0d0776ae44ed0cee90a0164e8a
William McKee <william@knowmad.com>
2014-08-03 16:28:43 -0400

    Really add the files

--- config.rb
+++ config.rb
@@ -28,6 +28,10 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+
+# Partials are stored with layouts
+set :partials_dir, 'layouts/partials'
+
 ###
 # Helpers
 ###
